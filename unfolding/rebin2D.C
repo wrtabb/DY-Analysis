966ecdf8e9ba38bb1c2021e8af6c21d0ecb1d2e9
Robert Tabb <wrtabb@gmail.com>
2020-07-29 12:39:38 -0500

    altered rebin2D to do some more tests

--- unfolding/rebin2D.C
+++ unfolding/rebin2D.C
@@ -3,22 +3,58 @@
 
 //forward declaration
 TH2D*Rebin2D(TH2D*hist,TString histName,std::vector<double> binning);
-TCanvas*MakeCanvas(TString cName);
+TCanvas*MakeCanvas(TString cName,bool log);
 void OutputEntries(TH2D*h1,TH2D*h2=0,TH2D*h3=0,TH2D*h4=0,TH2D*h5=0);
+void CheckBinByBin(TH2D*h1,TH2D*h2);
 
 TString fileName = "data/migrationMatrix.root";
 
-//double binning0[] = {0,5,10,15,20,25,30,35,40,45,50};
-//double binning1[] = {0,10,20,30,40,50};
-//std::vector<double> binningVec0 = {0,5,10,15,20,25,30,35,40,45,50};
-//std::vector<double> binningVec1 = {0,10,20,30,40,50};
-//int nBins0 = binningVec0.size()-1;
-//int nBins1 = binningVec1.size()-1;
+double binning0[] = {0,1,2,3,4,5,10,15,20,25,30,35,40,45,50};
+double binning1[] = {0,10,20,30,40,50};
+std::vector<double> binningVec0 = {0,1,2,3,4,5,10,15,20,25,30,35,40,45,50};
+std::vector<double> binningVec1 = {0,10,20,30,40,50};
+int nBins0 = binningVec0.size()-1;
+int nBins1 = binningVec1.size()-1;
 
 void rebin2D()
 {
  gStyle->SetPalette(1);
  gStyle->SetOptStat(0);
+
+ //-----Testing with toy models-----//
+ //Initialize histograms with different binnings on the y-axis
+ TH2D*h1 = new TH2D("h1","",nBins0,binning0,nBins1,binning1);
+ h1->SetTitle("h1");
+ TH2D*h2 = new TH2D("h2","",nBins0,binning0,nBins0,binning0);
+ h2->SetTitle("h2");
+
+ //Initialize random number generator to fill histograms
+ TRandom3 genX,genY;
+ genX.SetSeed(1827);
+ genY.SetSeed(8282);
+
+ //Set parameters for gaussian shapes for toy histograms 
+ double meanX = 25;
+ double meanY = 25;
+ double sigmaX = 5;
+ double sigmaY = 5;
+ double x,y;
+ int nEntries = 1e6;
+
+ //Fill histograms with same data
+ for(int i=0;i<nEntries;i++){
+  x = genX.Gaus(meanX,sigmaX);
+  y = genY.Gaus(meanY,sigmaY);
+  h1->Fill(x,y);
+  h2->Fill(x,y);
+ }
+
+ //Create rebinned version of h2 that matches h1
+ TH2D*h2Rebin = Rebin2D(h2,"h2Rebin",binningVec1);
+ h2Rebin->SetTitle("h2Rebin");
+
+ //-----Drell-Yan Distributions-----//
+ //File for opening Drell-Yan distributions
  TFile*file = new TFile(fileName);
 
  //Original migration matrix
@@ -30,60 +66,39 @@ void rebin2D()
  hMatrixRebin->SetTitle("Rebinned histogram");
 
  //Drawing the matrices on separate histograms for visual comparison
- TCanvas*c1 = MakeCanvas("c1");
- hMatrixM->Draw("colz");
- TCanvas*c2 = MakeCanvas("c2");
- hMatrixRebin->Draw("colz");
+ TCanvas*c1 = MakeCanvas("c1",false);
+ h1->Draw("colz");
+ TCanvas*c2 = MakeCanvas("c2",false);
+ h2Rebin->Draw("colz");
+ TCanvas*c3 = MakeCanvas("c3",false);
+ h2->Draw("colz");
 
  //Output of the number of entries in each histogram as a numerical comparison
- OutputEntries(hMatrixM,hMatrixRebin);
-
-
- /*
- TH2D*h1 = new TH2D("h1","",nBins0,binning0,nBins1,binning1);
- h1->SetTitle("h1");
- TH2D*h2 = new TH2D("h2","",nBins0,binning0,nBins0,binning0);
- h2->SetTitle("h2");
-
- TH2D*h3 = new TH2D("h3","",nBins0,binning0,100,0,50);
- h3->SetTitle("h3");
-
- TRandom3 genX,genY;
- genX.SetSeed(1827);
- genY.SetSeed(8282);
- double meanX = 25;
- double meanY = 25;
- double sigmaX = 5;
- double sigmaY = 5;
- double x,y;
- int nEntries = 1e6;
-
- for(int i=0;i<nEntries;i++){
-  x = genX.Gaus(meanX,sigmaX);
-  y = genY.Gaus(meanY,sigmaY);
-  h1->Fill(x,y);
-  h2->Fill(x,y);
-  h3->Fill(x,y);
- }
- TH2D*h2Rebin = Rebin2D(h2,"h2Rebin",binningVec1);
- h2Rebin->SetTitle("h2Rebin");
- TH2D*h3Rebin = Rebin2D(h3,"h3Rebin",binningVec1);
- h3Rebin->SetTitle("h3Rebin");
-
- TCanvas*c1 = MakeCanvas("c1");
- h1->Draw("colz");
- TCanvas*c2 = MakeCanvas("c2");
- h2->Draw("colz");
- TCanvas*c3 = MakeCanvas("c3");
- h3->Draw("colz");
- TCanvas*c2Rebin = MakeCanvas("c2Rebin");
- h2Rebin->Draw("colz");
- TCanvas*c3Rebin = MakeCanvas("c3Rebin");
- h3Rebin->Draw("colz");
-*/
-
+ OutputEntries(h1,h2Rebin);
+ CheckBinByBin(h1,h2);
 }
 
+void CheckBinByBin(TH2D*h1,TH2D*h2)
+{
+ int nBinsX = h1->GetNbinsX();
+ int nBinsY = h1->GetNbinsY();
+ if( (nBinsX != h2->GetNbinsX()) || (nBinsY != h2->GetNbinsY())){
+  cout << "---------------------------------" << endl;
+  cout << "ERROR: CheckBinByBin():" << endl;
+  cout << "Histogram bins do not match" << endl;
+  cout << "Cannot compare" << endl;
+  cout << "---------------------------------" << endl;
+  return;
+ }
+ for(int i=1;i<=nBinsX;i++){
+  for(int j=1;j<=nBinsY;j++){
+   cout << h1->GetBinContent(i,j) << ", " << h2->GetBinContent(i,j) << endl;
+   if(h1->GetBinContent(i,j) - h2->GetBinContent(i,j) != 0){
+    cout << "Bin " << i << ", " << j << " does not match!" << endl;
+   }//end if
+  }//end loop over Y
+ }//end loop over X
+}
 //Outputs number of entries for up to five histograms
 void OutputEntries(TH2D*h1,TH2D*h2=0,TH2D*h3=0,TH2D*h4=0,TH2D*h5=0)
 {
@@ -98,12 +113,14 @@ void OutputEntries(TH2D*h1,TH2D*h2=0,TH2D*h3=0,TH2D*h4=0,TH2D*h5=0)
 }
 
 //Makes canvases to draw on
-TCanvas*MakeCanvas(TString cName)
+TCanvas*MakeCanvas(TString cName,bool log)
 {
  TCanvas*canvas=new TCanvas(cName,"",0,0,1000,1000);
  canvas->SetGrid();
- canvas->SetLogx();
- canvas->SetLogy();
+ if(log){
+  canvas->SetLogx();
+  canvas->SetLogy();
+ }
  canvas->SetLogz();
  return canvas;
 }
@@ -115,8 +132,15 @@ TH2D*Rebin2D(TH2D*hist,TString histName,std::vector<double> binning)
  //As output with new Y-axis binning defined by input std::vector<double> binning
  
  int nBinsHistY = hist->GetNbinsY();//number of reco bins in the input histogram
- int nBinsHistX = hist->GetNbinsX();
+ int nBinsHistX = hist->GetNbinsX();//numner of true bins in the input histogram
+ double binningX[nBinsHistX];//true binning
  int nBinsReco = binning.size()-1;//number of reco bins for the output rebinned histogram
+
+ TH1D*histX = hist->ProjectionX();
+ for(int i=0;i<=nBinsHistX+1;i++){
+  if(i==0) binningX[i] = histX->GetBinLowEdge(i+1);
+  else binningX[i] = binningX[i-1]+histX->GetBinWidth(i);
+ }
  
  double newbinning[nBinsReco];//binning placed in array for defining TH2
  for(int i=0;i<=nBinsReco;i++){
@@ -124,7 +148,7 @@ TH2D*Rebin2D(TH2D*hist,TString histName,std::vector<double> binning)
  }
 
  //Initialize the output histogram
- TH2D*hRebin = new TH2D(histName,"",nBinsHistX,massbinsTrue,nBinsReco,newbinning);
+ TH2D*hRebin = new TH2D(histName,"",nBinsHistX,binningX,nBinsReco,newbinning);
 
  //Loop over each bin in the histogram
  //For each bin, define the position x and y from the bin center
